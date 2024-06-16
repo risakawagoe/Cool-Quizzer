9da37dd3e347d08510867e59dc24dba03d2663a7
Risa Kawagoe <risa.kawagoe.dev@gmail.com>
2024-06-15 18:26:12 -0700

    added env variables for config

--- cool-quizzer/src/controllers/firebase.js
+++ cool-quizzer/src/controllers/firebase.js
@@ -2,22 +2,22 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage"
 import { getFirestore } from "firebase/firestore"
 import { Quiz, QuizOverview } from "../models/Quiz";
-import { Question, QuestionType } from "../models/questions/Question";
+import { QuestionType } from "../models/questions/Question";
 import { MultipleChoiceQuestion } from "../models/questions/MultipleChoiceQuestion";
 import { ShortAnswerQuestion } from "../models/questions/ShortAnswerQuestion";
 import { NoAnswerQuestion } from "../models/questions/NoAnswerQuestion";
 import { MultipleSelectQuestion } from "../models/questions/MultipleSelectQuestion";
-import { convertToObject } from "typescript";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBxs3Bl4-17wIQSFqIcYdX0JuuCLiye6eY",
-  authDomain: "personal-projects-rkawagoe.firebaseapp.com",
-  projectId: "personal-projects-rkawagoe",
-  storageBucket: "personal-projects-rkawagoe.appspot.com",
-  messagingSenderId: "371949880356",
-  appId: "1:371949880356:web:466591be60164d6f6b84b1",
-  measurementId: "G-ECW93C4J6J"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -27,29 +27,10 @@ export const db = getFirestore(app);
 // Firestore data converter
 export const quizConverter = {
     toFirestore: (quiz) => {
-        console.log('in converter')
-        console.log(quiz)
         return {
             title: quiz.getTitle(),
             description: quiz.getDescription(),
             questions: quiz.getQuestions().map(question => { 
-                console.log(question)
-                console.log(question.getSerializableObject());
-                console.log(JSON.stringify(question))
-
-                // const keys = Object.keys(question);
-                // const obj = {};
-                // keys.forEach(key => obj[key] = question[key]);
-                // console.log(obj);
-                // console.log(obj["attachment"]);
-                // console.log(question["attachment"]);
-                // console.log(typeof question.attachment);
-                // console.log(question.attachment)
-                // console.log(question.getAttachment())
-                // if(typeof question.attachment === "string") {
-                //     obj.attachment = question.attachment;
-                // }
-
                 return { type: question.type, data: JSON.stringify(question.getSerializableObject()) };
             }),
             count: quiz.getQuestions().length,
@@ -58,17 +39,11 @@ export const quizConverter = {
     },
     fromFirestore: (snapshot, options) => {
         const data = snapshot.data(options);
-
-        console.log("data")
-        console.log(data)
-
         const hasAllFields = Object.keys(data).includes("title") && 
                                 Object.keys(data).includes("description") && 
                                 Object.keys(data).includes("stats") && 
                                 Object.keys(data).includes("questions");
         
-        console.log('hasAllFields')
-        console.log(hasAllFields)
 
         if(!hasAllFields) return null;
 
@@ -80,46 +55,33 @@ export const quizConverter = {
         if(Array.isArray(data.questions)) {
             data.questions.forEach(question => {
                 const typeInfoAvailable = question.type !== undefined && typeof question.type === "number";
-                console.log('type info available')
-                console.log(typeInfoAvailable)
                 if(typeInfoAvailable) {
                     let tmp = null;
-                    console.log('>> ' + question.type)
                     switch(question.type) {
                         case QuestionType.MULTIPLE_CHOICE:
-                            console.log('mc')
-                            
                             tmp = MultipleChoiceQuestion.deserialize(question.data);
                             break;
                         case QuestionType.MULTIPLE_SELECT:
-                            console.log('ms')
                             tmp = MultipleSelectQuestion.deserialize(question.data);
                             break;
                         case QuestionType.SHORT_ANSWER:
-                            console.log('short')
                             tmp = ShortAnswerQuestion.deserialize(question.data);
                             break;
                         case QuestionType.NO_ANSWER:
-                            console.log('no answer')
                             tmp = NoAnswerQuestion.deserialize(question.data);
                             break;
                         default:
-                            console.log('none')
                             // fall-through
                     }
                     if(tmp) {
                         quiz.addQuestion(tmp);
-                    }else {
-                        console.log('deserialization failed')
                     }
                 }
             })
         }else {
-            console.log('questions is not array')
             return null;
         }
 
-        console.log(quiz)
         return quiz;
     }
 };
@@ -139,25 +101,3 @@ export const quizOverviewConverter = {
         return overview;
     }
 };
-// export const questionConverter = {
-//     toFirestore: (question) => {
-//         const keys = Object.keys(question);
-//         const obj = {};
-//         keys.forEach(key => obj[key] = question[key]);
-//         console.log(obj);
-//         console.log(obj["attachment"]);
-//         return {
-//         };
-//     },
-//     fromFirestore: (snapshot, options) => {
-//         const data = snapshot.data(options);
-//         const overview = new QuizOverview(
-//             snapshot.id, 
-//             data.title, 
-//             data.description, 
-//             data.count, 
-//             data.stats
-//         );
-//         return overview;
-//     }
-// };
