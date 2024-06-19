34f6f8f270ddf209c1a68ef4a27c7e82f1c43cde
Risa Kawagoe <risa.kawagoe.dev@gmail.com>
2024-06-19 11:45:29 -0700

    updated backend quiz-service with cors

--- cool-quizzer-server/quiz-service/src/server.js
+++ cool-quizzer-server/quiz-service/src/server.js
@@ -1,16 +1,27 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
+const cors = require("cors");
 const { createQuiz, getQuiz, getAllQuizzes, updateQuiz, updateQuizStats, deleteQuiz, markShortAnswerQuestion } = require('./quiz-controller');
 
 const app = express();
 const port = 3000;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 
 // Endpoints
+// app.options("/api/*", function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Content-Type, Authorization, Content-Length, X-Requested-With"
+//     );
+//     res.send(200);
+// });
 app.get('/api/hello', (req, res) => {
     res.send('QUIZ_SERVICE: Hello!');
 });
